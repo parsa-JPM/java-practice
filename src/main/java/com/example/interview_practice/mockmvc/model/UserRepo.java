aa430027542dd389856bdaf8b6045971c5fcffe4
parsa-script <mihandoost.p@gmail.com>
2024-06-02 18:33:28 +0200

    add EntityGraph to load entity with single query

--- src/main/java/com/example/interview_practice/mockmvc/model/UserRepo.java
+++ src/main/java/com/example/interview_practice/mockmvc/model/UserRepo.java
@@ -1,8 +1,15 @@
 package com.example.interview_practice.mockmvc.model;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface UserRepo extends JpaRepository<User, Long> {
 
     User findByName(String name);
+
+    // to load this attribute with Join (single query) and avoid n+1 problem
+    @EntityGraph(attributePaths = {"sendTransactions", "recTransactions"})
+    List<User> findAll();
 }
