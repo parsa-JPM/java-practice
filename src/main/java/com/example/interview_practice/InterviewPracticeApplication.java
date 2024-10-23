e7178b33ac5056b29c2f3de913b0603e2aafa1f6
parsa-JPM <mihandoost.parsa@gmail.com>
2024-10-23 15:46:54 +0200

    add root mapping

--- src/main/java/com/example/interview_practice/InterviewPracticeApplication.java
+++ src/main/java/com/example/interview_practice/InterviewPracticeApplication.java
@@ -2,12 +2,19 @@ package com.example.interview_practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class InterviewPracticeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InterviewPracticeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewPracticeApplication.class, args);
+    }
 
+    @GetMapping("/")
+    public String rootPath() {
+        return "Everything works fine it seems :)";
+    }
 }
