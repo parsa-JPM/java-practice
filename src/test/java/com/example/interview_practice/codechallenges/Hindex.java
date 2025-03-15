6a6a8a419b1e3e482a89c48d67ac7de5a2bd545d
Joker <mihandoost.parsa@gmail.com>
2025-03-15 17:20:00 +0100

    add h-index code challenge

---
+++ src/test/java/com/example/interview_practice/codechallenges/Hindex.java
@@ -0,0 +1,65 @@
+package com.example.interview_practice.codechallenges;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static java.lang.Math.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Hindex {
+
+    @Test
+    void test() {
+        int[] input = {3, 0, 6, 1, 5};
+        assertEquals(3, hIndex(input));
+
+        int[] input2 = {1, 2, 3, 4, 5};
+        assertEquals(3, hIndexMap(input2));
+
+    }
+
+    private int hIndex(int[] citations) {
+        var hindex = 0;
+        for (int h = 1; h <= citations.length; h++) {
+            var matchH = 0;
+            for (int i = 0; i < citations.length; i++) {
+                if (citations[i] >= h) {
+                    matchH++;
+                }
+
+                if (matchH == h) {
+                    hindex = max(hindex, h);
+                    break;
+                }
+            }
+        }
+
+        return hindex;
+    }
+
+    private int hIndexMap(int[] citations) {
+        int n = citations.length;
+        int[] buckets = new int[n + 1];
+
+        for (int c : citations) {
+            buckets[min(c, n)]++;
+        }
+
+        int count = 0;
+        /*
+          bucket array would be like this:
+          value: 1 1 1 1 1
+          index: 1 2 3 4 5
+         */
+        for (int i = n; i >= 0; i--) {
+            count += buckets[i];
+            if (count >= i) {
+                return i;
+            }
+        }
+
+        return 0;
+    }
+}
