9d477f6034416db1a28cce69b9834958a7253d03
Joker <mihandoost.parsa@gmail.com>
2026-06-10 18:32:04 +0200

    make chat client with ollama

---
+++ src/main/java/com/example/interview_practice/ai/ChatController.java
@@ -0,0 +1,46 @@
+package com.example.interview_practice.ai;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping("/ai")
+public class ChatController {
+
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
+
+    @GetMapping("/joke")
+    public String getJoke() {
+        return chatClient.prompt()
+                .user("Tell me a funny programming joke.")
+                // blocking call
+                .call()
+                .content();
+    }
+
+    @GetMapping("/stream")
+    public Flux<String> stream(){
+        return chatClient.prompt()
+                .user("I'm visiting London soon, can you give me 10 places I must visit?")
+                // non-blocking call which returns Flux from project reactor (spring web-flux)
+                .stream()
+                .content();
+    }
+
+    @GetMapping("/detail")
+    public ChatResponse detail(){
+        return chatClient.prompt()
+                .user("Tell me a funny programming joke.")
+                // blocking call
+                .call()
+                .chatResponse();
+    }
+}
