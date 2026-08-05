e879c5a9aecb2732c673c6d5808c9b716867ceb0
Joker <mihandoost.parsa@gmail.com>
2026-08-05 21:41:41 +0200

    capital.com code challenge

--- src/main/java/com/example/interview_practice/capital/service/ReservationService.java
+++ src/main/java/com/example/interview_practice/capital/service/ReservationService.java
@@ -21,6 +21,18 @@ public class ReservationService {
         this.reservations = reservations;
     }
 
+    /**
+     * Create a restaurant reservation system.
+     * The restaurant has 10 tables with different capacities.
+     * Functionality:
+     *
+     * 1)Add an ability to reserve a table by providing party size and time slot (hourly slots only, e.g., 18:00, 19:00, 20:00). Returns the reserved table ID.
+     *
+     * 2)Add an ability to cancel a reservation by providing table ID and time slot.
+     *
+     * Each table can only have one reservation per time slot. Always assign the smallest available table that fits the party.
+     *
+     */
     public int reserve(int partySize, LocalTime startTime){
 
         // check party size with table size
